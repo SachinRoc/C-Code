00ef0caec5aa1f8d5e3d39dff57a153d342600da
sachin <sachinkashyap80770@gmail.com>
2023-04-23 17:08:19 +0530

    Adding print function of stack

--- stackCreation.cpp
+++ stackCreation.cpp
@@ -35,7 +35,18 @@ else{
 }
 
 
+// //Print the Element of stack;
+// stack<int> s ;
 
+// s.push(10);
+// s.push(20);
+// s.push(30);
+// s.push(40);
+
+// while(!s.empty()){
+//     cout<<s.top()<< " ";
+//     s.pop();
+// }
 
     return 0;
 }
